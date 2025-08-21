0e33bbc71f050545ac9416e10591c73880a344b6
quanghiep03198 <quanghiep03198@gmail.com>
2025-08-21 16:28:53 +0700

    fix: clean up build and distribution directories before building

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,3 +1,6 @@
 :: filepath: /d:/Workspace/My Projects/epc-combiner-tool/build.bat
+if exist build rmdir /s /q build
+if exist dist rmdir /s /q dist
+
 @echo off
 pyinstaller main.py --windowed --noconsole --onedir --contents-directory . --name="EPC Information Combiner" --add-data="app.cfg;." --add-data="icon.ico;." --add-data="assets;assets" --add-data="themes/*.qss;themes" --add-data="repositories/sql;repositories/sql" --icon=icon.ico
