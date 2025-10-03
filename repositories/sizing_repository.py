ecc8e6450b41f33db6f335ce58bd701452957cca
quanghiep03198 <quanghiep03198@gmail.com>
2025-10-03 16:29:53 +0700

    refactor: streamline database connection handling in SizingRepository and improve SQL query structure

--- repositories/sizing_repository.py
+++ repositories/sizing_repository.py
@@ -1,8 +1,10 @@
 from PyQt6.QtSql import *
 from helpers.logger import logger
 from pathlib import Path
-from database import DatabaseService, DataSources, DatabaseConnection
+from database import db_service, DataSources, DatabaseConnection
 from helpers.configuration import ConfigService
+
+# from database import DATA_SOURCE_ERP
 
 configuration = ConfigService.load_configs()
 
@@ -10,46 +12,13 @@ configuration = ConfigService.load_configs()
 class SizingRepository:
     __sql_file_path = Path(__file__).parent.resolve() / "./sql/get_size_qty.sql"
 
-    __ERP_DB_CONN__ = DatabaseService.connnect_database(
-        server=configuration.get("DB_SERVER"),
-        database=DataSources.ERP.value,
-        connection_name=f"[SizingRepository]{DatabaseConnection.ERP.value}",
-    )
-    """
-        A new instance of connection to ERP database to execute parallel queries
-    """
-
     @staticmethod
     def find_size_qty(params: dict) -> list[dict]:
-        result = []
-        query = None
-        try:
-            sql_statement = DatabaseService.get_raw_sql(
-                SizingRepository.__sql_file_path
-            )
-            query = QSqlQuery(SizingRepository.__ERP_DB_CONN__)
-            query.prepare(sql_statement)
-            query.bindValue(":mo_no", params["mo_no"])
-            if params["mo_noseq"] == "all":
-                query.bindValue(":mo_noseq", None)
-            else:
-                query.bindValue(":mo_noseq", params["mo_noseq"])
-            query.exec()
-            while query.next():
-                result.append(
-                    {
-                        "size_code": query.value("size_code"),
-                        "size_numcode": query.value("size_numcode"),
-                        "size_qty": query.value("size_qty"),
-                        "combined_qty": query.value("combined_qty"),
-                        "in_use_qty": query.value("in_use_qty"),
-                        "compensated_qty": query.value("compensated_qty"),
-                        "cancelled_qty": query.value("cancelled_qty"),
-                    }
-                )
-        except Exception as e:
-            logger.error(f"Error finding sizing detail: {e}")
-        finally:
-            if query is not None:
-                query.finish()
-            return result
+        return db_service.execute_query(
+            connection_type=DatabaseConnection.ERP,
+            sql_query=db_service.get_raw_sql(SizingRepository.__sql_file_path),
+            bind_values={
+                "mo_no": params["mo_no"],
+                "mo_noseq": None if params["mo_noseq"] == "all" else params["mo_noseq"],
+            },
+        )
